4828fb8ab35461a1bc715530b108731533937f7b
jperez <jperez.a4b@gmail.com>
2018-11-29 19:31:15 +0100

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+router.get("/", (req, res, next) => {
+  res.render("signin");
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+router.post("/signup", passport.authenticate("local-signup", {
+    successRedirect: "/profile",
+    failureRedirect: "/signup",
+    passReqToCalback: true
+  })
+);
+
+router.get("/signin", (req, res, next) => {
+  res.render("signin");
+});
+
+router.post("/signin",passport.authenticate("local-signin", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/signin",
+    passReqToCalback: true
+  })
+);
+
+router.get("/logout", (req, res, next) => {
+  req.logout();
+  res.redirect("/");
+});
+
+router.get("/profile", isAuthenticated, (req, res, next) => {
+  res.render("profile");
+});
+
+router.get("/dashboard", isAuthenticated, (req, res, next) => {
+  res.render("dashboard");
+});
+
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+module.exports = router;
